14246f830a81b057e58ff250a9bdd8367ca33f8d
LinGill21 <42749226+LinGill21@users.noreply.github.com>
2020-02-13 15:20:35 -0600

    javascript works

--- scripts/lingill.js
+++ scripts/lingill.js
@@ -5,39 +5,44 @@ const calculationHold = document.getElementById("calcuation")
 const tipHold =document.getElementById("tip")
 const taxhold = document.getElementById("tax")
 
-inputTotal.onclick = function () {
-    alert("thanks for clicking input 1!");
-  };
   
 //gerneral call to functions and returning
-btnCalculate.onClick = async function(){
+btnCalculate.onclick = async function(){
     console.log('INFO: Declaring functions');
     const totalstr =inputTotal.value;
     const ratestr =inputRate.value;
     const totalNum =parseInt(totalstr);
     const rateNum =parseInt(ratestr);
-    //const tipNum = await tip(totalNum, rateNum);
-    const tipNum = (totalNum*rateNum).toFixed(2);
-    tipHold.innerHTML = tipNum;
+    //tip
+    const tipNum = await tip(totalNum, rateNum);
+    tipHold.innerHTML = "Tip: "+tipNum;
+    //tax
     const taxNum = await tax( totalNum);
-    taxhold.innerHTML = taxNun;
+    taxhold.innerHTML = "Tax: "+taxNum;
+    // sum
     const grand = await sum(totalNum, tipNum, taxNum);
-    calculationHold.innerHTML = grand;
+    calculationHold.innerHTML = "Grand total: "+grand;
 }
+//found .fixed() at https://www.w3schools.com/jsref/jsref_tofixed.asp
 //tip calc
 const tip = async function ( totalNum, rateNum){
-  const giventip = totalNum *rateNum
+  percent =rateNum/100;
+  const giventip = totalNum *percent;
   return giventip.toFixed(2);
 
 }
 
 // tax calc
-//found .fixed() at https://www.w3schools.com/jsref/jsref_tofixed.asp
 const tax =async function (totalNum){
     const totaltax =(totalNum * 0.055);
     return totaltax.toFixed(2);
 }
 //sum calc
 const sum = async function (totalNum, tipNum, taxNum){
-    return totalNum + tipNum + taxNum;
+  // somehow they are stings
+    totalSum1 = parseFloat(totalNum);
+    totalSum2 = parseFloat(tipNum)
+    totalSum3 = parseFloat(taxNum)
+    totalSum = totalSum1 + totalSum2 + totalSum3;
+    return totalSum;
 }
